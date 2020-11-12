966334f9c3bf1cc79f173159611658e4b6a671f4
madhavi125 <60025683+madhavi125@users.noreply.github.com>
2020-11-12 15:59:52 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,44 @@
+<?php require_once('connect.php');
+
+session_start();
+?>
+<html>
+    <head>
+        <title>About Us</title>
+        
+        <link rel="stylesheet" type="text/css" href="../css/aboutus.css">
+        
+    </head>
+
+    <body>
+    <?php include('homenavbar.php');
+        
+        
+        ?>
+        <div class="main">
+        <div class="loginbox">
+            <center><h1 >About Mr.BEE</h1></center>
+            <h3>Our History</h3>
+            <p>
+            </p>
+            
+           
+            <h3>Our Mission</h3>
+            <p>Uplift the current record keeping and management methods of local beekeepers under Nature Bee Honey Company Sri Lanka and offer the sales products to meet a variety of customers and raise awareness of these products via online marketing.
+            </p>
+
+            <h3>Our Focus</h3>
+            <p>To give an opportunity to local beekeepers to Manage their constant record keeping of his/her bee honey production
+        and Assisting in sustaining and managing one’s own beehives and apiaries.</p>
+        <br/>
+<p>To provide an online marketing platform for the sales products.
+
+            </p>
+            <h3>Our Team </h3>
+            <p>We are an experienced, passionate, creative team, who are dedicated 
+                to providing your business with the best experience.</p>
+            
+        </div>
+        </div>
+    <body>
+</html>
