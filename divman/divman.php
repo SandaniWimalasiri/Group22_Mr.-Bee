5be777bb3794b6b650c5d5f837293306f893413f
Malaka Hewawasam <56576219+HewawasamM@users.noreply.github.com>
2020-11-12 13:51:43 +0530

    divisional manager:without php

---
+++ divman/divman.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Divisional_Manager</title>
+	<!--link to css file-->
+	<link rel="stylesheet" type="text/css" href="css/main.css"/>
+</head>
+<body>
+	<!--main-->
+	<section id="main">
+		<!--navigation-->
+		<nav>
+			<!--logo-->
+			<a href="#" class="logo">Mr.<font color="#f4976c">Bee</font></a>
+			<!--menu-->
+			<ul class="menu">
+				<li><a href="#main">Home</a></li>
+				<li><a href="#about">Profile</a></li>
+				<li><a href="#beekeeper">Bee Keepers</a></li>
+				<li><a href="#reports">Reports</a></li>
+				<li><a href="#info">Info-Hub</a></li>
+			</ul>
+			<a href="#" class="lang">Logout</a>
+		</nav>
+		<div class="name">
+			<p class="details">Welcome...</p>
+			<h1>Malaka Hewawasam</h1>
+			<p class="details">Divisional Manager</br>Nature Bee Honey Company SriLanka</p>
+		</div>
+		<div class="hcmenue">
+		<ul class ="honeycomb">
+			<li class="honeycomb-cell">
+				<a href="#beekeeper">
+				<img class="honeycomb-cell_img" src="image/011.jpg">
+				<div class="honeycomb-cell_title">Bee Keepers</div>
+				</a>
+			</li>
+			<li class="honeycomb-cell">
+				<a href="#reports">
+				<img class="honeycomb-cell_img" src="image/012.jpg">
+				<div class="honeycomb-cell_title">Reports</div>
+				</a>
+			</li>
+			<li class="honeycomb-cell">
+				<a href="#info">
+				<img class="honeycomb-cell_img" src="image/018.jpg">
+				<div class="honeycomb-cell_title">Info-Hub</div>
+				</a>
+			</li>
+			<li class="honeycomb-cell honeycomb_Hidden">
+			</li>
+		</ul>
+
+		</div>
+		
+	</section>
+
+	<!--about-->
+
+	<section id="about">
+		<div class="about-text">
+			<h1>Profile</h1>
+			<h2>Malaka Hewawasam</h2>
+			<p>abo utu sa bout usaboutu sabo utusa boutu sabout usabou tusa bou tusab outus abou tusab outus abou tus abou tusa bout usa bout usab outus abou tusab outus abou tusa bou tusab outu sabo utu sabout usabo utusa bout usab outu sabo utusa bout usa boutusa bout usabou tusabo utus abou tus abo ut u sabou tusabo utus abou tus abou tusa bou tu s</p>
+
+		<button>Edit Profile</button>
+		</div>
+		<div class="about-model">
+			<img src="image/007.png" alt="me"/>
+			
+		</div>
+	</section>
+	<!--beekeeper-->
+	<section id="beekeeper">
+	<!--beekeeper heading-->
+	<div class="b-heading">
+		<h1>Bee keepers</h1>
+	</div>
+		
+	</section>
+	<!--Report-->
+	<section id="reports">
+	<!--beekeeper heading-->
+	<div class="r-heading">
+		<h1>Reports</h1>
+	</div>
+		
+	</section>
+
+	<!--info-->
+	<section id="info">
+	<!--info heading-->
+	<div class="i-heading">
+		<h1>Infomation Hub</h1>
+	</div>
+	<!--info box container-->
+	<div class="i-b-conatainer">
+	<!--box1-->
+	<div class="i-box">
+	<!--info box image-->
+	<div class="i-b-img">
+	<!--type-->
+	<div class="i-type">Info-Hub</div>
+	<!--img-->
+	<img src="image/i1.jpg">
+	</div>
+	<!--info box text-->	
+	<div class="i-b-text">
+		<a href="#">se rvic esse rvic esser vices serv ices servic esse rvic esse rvi ces servi cesservi cesservi ces ser vice sservi cesse rvice sser vices serv ices servi cesser vices servic esser vicesservic esservi cesse rvice sser vice sse rv i ce sser vices ser vice s s er vicesse rvi cess erv i ce s s e rvice sservice sserv icess ervi cesservic esser vic es s erv ice ss erv ic es</a>
+	</div>	
+	
+	</div>
+	<!--box1-->
+	<div class="i-box">
+	<!--info box image-->
+	<div class="i-b-img">
+	<!--type-->
+	<div class="i-type">Info-Hub</div>
+	<!--img-->
+	<img src="image/i2.jpg">
+	</div>
+	<!--info box text-->	
+	<div class="i-b-text">
+		<a href="#">se rvic esse rvic esser vices serv ices servic esse rvic esse rvi ces servi cesservi cesservi ces ser vice sservi cesse rvice sser vices serv ices servi cesser vices servic esser vicesservic esservi cesse rvice sser vice sse rv i ce sser vices ser vice s s er vicesse rvi cess erv i ce s s e rvice sservice sserv icess ervi cesservic esser vic es s erv ice ss erv ic es</a>
+	</div>	
+	
+	</div>
+	<!--box1-->
+	<div class="i-box">
+	<!--info box image-->
+	<div class="i-b-img">
+	<!--type-->
+	<div class="i-type">Info-Hub</div>
+	<!--img-->
+	<img src="image/i3.jpg">
+	</div>
+	<!--info box text-->	
+	<div class="i-b-text">
+		<a href="#">se rvic esse rvic esser vices serv ices servic esse rvic esse rvi ces servi cesservi cesservi ces ser vice sservi cesse rvice sser vices serv ices servi cesser vices servic esser vicesservic esservi cesse rvice sser vice sse rv i ce sser vices ser vice s s er vicesse rvi cess erv i ce s s e rvice sservice sserv icess ervi cesservic esser vic es s erv ice ss erv ic es</a>
+	</div>	
+	
+	</div>
+	<!--box1-->
+	<div class="i-box">
+	<!--info box image-->
+	<div class="i-b-img">
+	<!--type-->
+	<div class="i-type">Info-Hub</div>
+	<!--img-->
+	<img src="image/i4.jpg">
+	</div>
+	<!--info box text-->	
+	<div class="i-b-text">
+		<a href="#">se rvic esse rvic esser vices serv ices servic esse rvic esse rvi ces servi cesservi cesservi ces ser vice sservi cesse rvice sser vices serv ices servi cesser vices servic esser vicesservic esservi cesse rvice sser vice sse rv i ce sser vices ser vice s s er vicesse rvi cess erv i ce s s e rvice sservice sserv icess ervi cesservic esser vic es s erv ice ss erv ic es</a>
+	</div>	
+	
+	</div>
+	</div>
+	<a href="#" class=i-btn>View More</a>
+	</section>
+
+
+
+
+
+</body>
+</html>
