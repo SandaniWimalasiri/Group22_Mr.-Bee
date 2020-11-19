89d87ca13c6cad64a82b56b4b61b279504d123a4
SandaniWimalasiri <sandaniwimalasiri@gmail.com>
2020-11-20 00:23:59 +0530

    changed aboutus UI

--- php/aboutus.php
+++ php/aboutus.php
@@ -16,29 +16,87 @@ session_start();
         
         ?>
         <div class="main">
-        <div class="loginbox">
-            <center><h1 >About Mr.BEE</h1></center>
-            <h3>Our History</h3>
-            <p>
-            </p>
-            
-           
-            <h3>Our Mission</h3>
-            <p>Uplift the current record keeping and management methods of local beekeepers under Nature Bee Honey Company Sri Lanka and offer the sales products to meet a variety of customers and raise awareness of these products via online marketing.
-            </p>
+    
 
-            <h3>Our Focus</h3>
-            <p>To give an opportunity to local beekeepers to Manage their constant record keeping of his/her bee honey production
-        and Assisting in sustaining and managing one’s own beehives and apiaries.</p>
-        <br/>
-<p>To provide an online marketing platform for the sales products.
 
-            </p>
-            <h3>Our Team </h3>
-            <p>We are an experienced, passionate, creative team, who are dedicated 
-                to providing your business with the best experience.</p>
-            
+
+         <!--info box container-->
+	<div class="i-b-conatainer">
+	<!--box1-->
+	<div class="i-box">
+	<!--info box image-->
+	<div class="i-b-img">
+	<!--type-->
+	<div class="i-type">Our Mission</div>
+	<!--img-->
+	<img src="../img/bee comb.jpg">
+	</div>
+	<!--info box text-->	
+	<div class="i-b-text">
+    </br>
+        <p>     Uplift the current record keeping and management methods of local beekeepers under Nature Bee Honey Company Sri Lanka and offer the sales products to meet a variety of customers and raise awareness of these products via online marketing.
+        </p>
+    </div>	
+	
+	</div>
+	<!--box1-->
+	<div class="i-box">
+	<!--info box image-->
+	<div class="i-b-img">
+	<!--type-->
+	<div class="i-type">Our Focus</div>
+	<!--img-->
+	<img src="../img/bee-keeping.png">
+	</div>
+	<!--info box text-->	
+	<div class="i-b-text">
+    </br>
+    <p>     To give an opportunity to local beekeepers to Manage their constant record keeping of his/her bee honey production
+        and Assisting in sustaining and managing one’s own beehives and apiaries.
+</br>To provide an online marketing platform for the sales products.
+    </p>
+            </div>	
+	
+	</div>
+	<!--box1-->
+	<div class="i-box">
+	<!--info box image-->
+	<div class="i-b-img">
+	<!--type-->
+	<div class="i-type">Our History</div>
+	<!--img-->
+	<img src="../img/honey3.jpg">
+	</div>
+	<!--info box text-->	
+	<div class="i-b-text">
+    </br>
+        <p>     se rvic esse rvic esser vices serv ices servic esse rvic esse rvi ces servi cesservi cesservi
+         ces ser vice sservi cesse rvice sser vices serv ices servi cesser vices servic esser vicesservic
+          esservi cesse rvice sser vice sse rv i ce sser vices ser vice s s er vicesse rvi cess erv i ce s 
+          s e rvice sservice sserv icess ervi cesservic esser vic es s erv ice ss erv ic es</p>
+	</div>	
+	
+	</div>
+	<!--box1-->
+	<div class="i-box">
+	<!--info box image-->
+	<div class="i-b-img">
+	<!--type-->
+	<div class="i-type">Our Team</div>
+	<!--img-->
+	<img src="../img/10.png">
+	</div>
+	<!--info box text-->	
+	<div class="i-b-text">
+    </br><p>        We are an experienced, passionate, creative team, who are dedicated 
+                to providing your business with the best experience.
+    </p>	
+	
+    </div>
+    </div>
         </div>
-        </div>
+
+
+       
     <body>
 </html>
