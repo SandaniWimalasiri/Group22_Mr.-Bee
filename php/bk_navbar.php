378c90118a5b7310fd7238a38b9533d955aa83b2
madhavi125 <msellahannadi@gmail.com>
2020-11-22 10:37:00 +0530

    changed bk_navbar.php

--- php/bk_navbar.php
+++ php/bk_navbar.php
@@ -53,13 +53,12 @@ $result = mysqli_query($connection,$sql);
 </div>
 
   <script>
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
+
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
+
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
