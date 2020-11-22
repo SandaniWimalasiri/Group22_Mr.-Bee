b6263d79dc172bf1693a88d16dcd2afbf9c3ed94
madhavi125 <msellahannadi@gmail.com>
2020-11-22 10:45:33 +0530

    changed navbar.php

--- php/homenavbar.php
+++ php/homenavbar.php
@@ -23,13 +23,12 @@
 </div> 
 
   <script>
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
+
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
+
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
