0f50124c3e9e0f866db49f36e65944c68a632f32
madhavi125 <msellahannadi@gmail.com>
2020-11-22 10:21:05 +0530

    changed aboutus.php

--- php/aboutus.php
+++ php/aboutus.php
@@ -20,34 +20,34 @@ session_start();
 
 
 
-         <!--info box container-->
+         
 	<div class="i-b-conatainer">
-	<!--box1-->
+	
 	<div class="i-box">
-	<!--info box image-->
+	
 	<div class="i-b-img">
-	<!--type-->
+	
 	<div class="i-type">Our Mission</div>
-	<!--img-->
+	
 	<img src="../img/bee comb.jpg">
 	</div>
-	<!--info box text-->	
+		
 	<div class="i-b-text">
     </br>
         <p>     Our sole purpose is to spread knowledge among the people as well as to launch new products to make our vision a reality.</p>
     </div>	
 	
 	</div>
-	<!--box1-->
+	
 	<div class="i-box">
-	<!--info box image-->
+	
 	<div class="i-b-img">
-	<!--type-->
+	
 	<div class="i-type">Our Vision</div>
-	<!--img-->
+	
 	<img src="../img/bee-keeping.png">
 	</div>
-	<!--info box text-->	
+		
 	<div class="i-b-text">
     </br>
     <p>    Our endeavor is to launch a number of new projects that will bring huge economic benefits to the Republic of Sri Lanka through beekeeping.
@@ -55,16 +55,16 @@ session_start();
             </div>	
 	
 	</div>
-	<!--box1-->
+	
 	<div class="i-box">
-	<!--info box image-->
+	
 	<div class="i-b-img">
-	<!--type-->
+	
 	<div class="i-type">Our History</div>
-	<!--img-->
+	
 	<img src="../img/honey3.jpg">
 	</div>
-	<!--info box text-->	
+		
 	<div class="i-b-text">
     </br>
         <p>     It is a fact that we all know that our lives have developed by absorbing the nutrients of this fertile earth. Therefore, 
@@ -75,16 +75,16 @@ session_start();
 	</div>	
 	
 	</div>
-	<!--box1-->
+	
 	<div class="i-box">
-	<!--info box image-->
+	
 	<div class="i-b-img">
-	<!--type-->
+	
 	<div class="i-type">Our Team</div>
-	<!--img-->
+	
 	<img src="../img/10.png">
 	</div>
-	<!--info box text-->	
+		
 	<div class="i-b-text">
     </br><p>        We are an experienced, passionate, creative team, who are dedicated 
                 to providing your business with the best experience.
