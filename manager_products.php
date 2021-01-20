388860b8d20ad03a03cf3a2aad67388143fb250e
SandaniWimalasiri <56576468+SandaniWimalasiri@users.noreply.github.com>
2021-01-20 16:18:33 +0530

    new uploads

---
+++ manager_products.php
@@ -0,0 +1,127 @@
+<?php include('manager_alignments.php')?>
+<?php include('manager_navbar.php')?>
+
+<?php
+			if(isset($_POST['delete'])){
+				$sql = "DELETE FROM products WHERE id = '{$_POST['id']}';";
+				$result = mysqli_query($connection,$sql);
+				if($result){
+				    header("Location:manager_products.php ");
+				}
+				else{
+                    echo "failed";
+				}
+			}
+?>
+
+<php >
+    <head>
+        
+    <title>Mr. Bee</title>
+        <link rel="stylesheet" type="text/css" href="../../public/css/style_manager_homepage.css">
+        <link rel="stylesheet" type="text/css" href="../../public/css/style_buttons.css">
+        <link rel="stylesheet" type="text/css" href="../../public/css/style_manager_products.css">
+        <link rel="stylesheet" type="text/css" href="../../public/css/style_manager_remove_dm.css">
+
+    </head>
+    <body>
+       
+
+       
+
+         <!--start content-->
+        <div class="content">      
+             <div class="viewform">
+                    
+                        <div class="dropdown">
+                            <button class="dropbtn">Products</button>
+                            <div class="dropdown-content">
+                                <a href="manager_products_view.php">Product List</a>
+                                <a href="manager_products_add.php">Add a Product</a>
+                                
+                            </div>
+                        </div>
+                       
+                    <br>
+             </div>  
+            
+            <center>
+                <div class="viewform">
+                    <form method="post">
+                        <br><br>
+                        <label> Search  </label>
+                        <input type="text" name="code">
+                        
+                        <button class="btn6" type="submit" name="view"><b>View</b></button>
+                        
+                       
+                    </form>
+                </div>    		
+              </center>
+
+            <table class="div_man">
+                
+                
+                <?php
+                    if(isset($_POST['view'])){
+                        echo"<tr>
+                        <th>Product code</th>
+                        <th>Product name</th>
+                        <th>Description</th>
+                        <th>Discounted Price (Rs.)</th>
+                        <th>Price (Rs.)</th>
+                        <th>Amount in list</th>
+                        <th>image</th>
+                        <th>date-added</th>
+                        <th>Edit</th>
+                        <th>Delete</>
+                        
+                        </tr>";
+                        $code =mysqli_real_escape_string($connection,$_POST['code']);
+        
+                        $sql = "SELECT * FROM products WHERE id='{$_POST['code']}' OR pname='{$_POST['code']}' OR rrp='{$_POST['code']}' OR quantity='{$_POST['code']}' ;";
+                        $query = $connection->query($sql);
+                        verify_query($query);
+                        while ($result = $query->fetch_array()){
+                ?>
+                        <tr>
+                            <td><?php echo $result['id'] ?></td>
+                            <td><?php echo $result['pname'] ?></td>
+                            <td><?php echo $result['descr'] ?></td>
+                            <td><?php echo $result['price'] ?></td>
+                            <td><?php echo $result['rrp'] ?></td>
+                            <td><?php echo $result['quantity'] ?></td>
+                            <td><?php echo $result['img'] ?></td>
+                            <td><?php echo $result['date_added'] ?></td>
+                            <form action="manager_products_update.php" method="post">
+                                    <input type="hidden" name="id" value="<?php echo $result['id']; ?>">
+                                    <td><button class="btn8" type="submit">Edit</a></td>
+                            </form>
+                            <form action="manager_products_delete.php" method="post">
+                                    <input type="hidden" name="id" value="<?php echo $result['id']; ?>">
+                                    <td><button class="btn7" type="submit" name="delete" onclick="return confirm('Are you sure?')">Delete</a></td>
+                            </form>
+                        </tr>
+                        <?php
+                         }
+                    
+                ?>  
+                <?php
+                         
+                    }
+                ?>
+
+            </table>
+
+              
+        </div>               <!--end content-->
+
+       
+
+        <!--start of footer-->
+        <footer>    
+            <h3></h3>
+        </footer>   <!--end of footer-->
+
+    </body>
+</php>
---
+++ manager_products_update.php
@@ -0,0 +1,114 @@
+<?php require_once("../../config/connect.php");
+require_once("func.php"); ?>
+
+<?php include('manager_alignments.php')?>
+<?php include('manager_navbar.php')?>
+
+<?php session_start(); 
+
+        if(!$_SESSION['userName']){
+            header('Location: sign_in_admin.php');
+        }
+?>
+
+    <head>
+        
+    <title>Mr. Bee</title>
+        <link rel="stylesheet" type="text/css" href="../../public/css/style_manager_homepage.css">
+        <link rel="stylesheet" type="text/css" href="../../public/css/style_buttons.css">
+        <link rel="stylesheet" type="text/css" href="../../public/css/style_manager_remove_dm.css">
+
+    </head>
+
+    <style>
+    
+    </style>
+<body>
+   
+
+        
+
+        <div class="content"> 
+            <?php
+                $id=$_POST['id'];
+
+                $query = "SELECT * FROM products WHERE id='$id' ";
+                $query_run = mysqli_query($connection, $query);
+
+                if($query_run){
+                    while($row = mysqli_fetch_array($query_run)){
+                    ?>
+                        <h1 >Update Products' Details</h1>
+                        <button class="btn6" type="submit" name="back" onclick="document.location='manager_products_view.php'"><<<b>Back</b></button>
+
+                        <form class="f1" action="manager_products_update.php" method="post">
+                        </br>
+                        <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                            
+                            <table class="div_man">
+                            <tr>
+                                <th><label>product name</label></th>
+                                <td><input type="text" name="pname" value="<?php echo $row['pname'] ?>"></td>
+                            </tr>
+                            <tr>
+                                <th><label>product Description</label></th>
+                                <td><input type="text" name="descr" value="<?php echo $row['descr'] ?>"></td>
+                            </tr>
+                            <tr>
+                                <th><label>Discounted Price (Rs.)</label></th>
+                                <td><input type="text" name="rrp" value="<?php echo $row['rrp'] ?>"></td>
+                            </tr>
+                            <tr>
+                                <th><label>price</label></th>
+                                <td><input type="text" name="price" value="<?php echo $row['price'] ?>"></td>
+                            </tr>
+                            <tr>
+                                <th><label>quantity</label></th>
+                                <td><input type="text" name="quantity" value="<?php echo $row['quantity'] ?>"></td>
+                            </tr>
+                            <tr>
+                                <th><label>image</label></th>
+                                <td><input type="text" name="img" value="<?php echo $row['img'] ?>"></td>
+                            </tr>
+                            
+                            </table>
+                            <br/>
+
+                            <button class="btn6" type="submit" name="update"><b>Update</b></button>
+                        </form>
+           
+                    <?php }   ?> 
+                        <?php
+                                if(isset($_POST['update']))
+                                {
+                                    $pname = $_POST['pname'];
+                                    $descr = $_POST['descr'];
+                                    $price = $_POST['price'];
+                                    $rrp = $_POST['rrp'];
+                                    $quantity = $_POST['quantity'];
+                                    $img = $_POST['img'];
+
+                                    $query = "UPDATE products SET pname='$pname',descr='$descr', price='$price', rrp='$rrp', quantity='$quantity', img='$img'  WHERE id='$id'  ";
+                                    $query_run = mysqli_query($connection, $query);
+
+                                    if($query_run)
+                                    {
+                                        echo '<script> alert("Data Updated Successfully"); </script>';
+                                        header("location:manager_products_view.php");
+                                    }
+                                    else
+                                    {
+                                        echo '<script> alert("Data Not Updated"); </script>';
+                                    }
+                                }
+                        ?>
+                        <?php
+                    
+                   
+                }
+                ?>
+        </div>               <!--end content2-->
+
+        
+</body>
+
