ef2dd7233fba79f952cac923c0103883de360b5c
SandaniWimalasiri <sandaniwimalasiri@gmail.com>
2021-01-20 13:39:54 +0530

    new

---
+++ src/php/manager_orders.php
@@ -0,0 +1,38 @@
+<?php include('manager_alignments.php')?>
+<?php include('manager_navbar.php')?>
+
+<php >
+    <head>
+        
+    <title>Mr. Bee</title>
+        <link rel="stylesheet" type="text/css" href="../../public/css/style_manager_homepage.css">
+        <link rel="stylesheet" type="text/css" href="../../public/css/style_buttons.css">
+
+    </head>
+    <body>
+        
+
+        
+
+         <!--start content-->
+        <div class="content">   
+                    <div class="dropdown">
+                        <button class="dropbtn">Order Details </button>
+                        <div class="dropdown-content">
+                            <a href="">Ongoing Orders</a>
+                            
+                            <a href="" name="enter">completed orders</a>
+                            </form>
+                            
+                        </div>
+                    </div>
+           
+        </div>               <!--end content-->
+
+        <!--start of footer-->
+        <footer>    
+            <h3></h3>
+        </footer>   <!--end of footer-->
+
+    </body>
+</php>
