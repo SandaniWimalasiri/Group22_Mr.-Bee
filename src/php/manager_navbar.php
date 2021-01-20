d95c284444597908060ee1effe507adff709f728
SandaniWimalasiri <sandaniwimalasiri@gmail.com>
2021-01-20 13:38:59 +0530

    updated

--- src/php/manager_navbar.php
+++ src/php/manager_navbar.php
@@ -1,69 +1,68 @@
 
 
     <head>
+        <meta charset="utf-8">
+        <title>admin page</title>
         <link rel="stylesheet" type="text/css" href="../../public/css/style_manager_homepage.css">
         <link rel="stylesheet" type="text/css" href="../../public/css/style_manager_navbar.css">
-
+        <link rel="stylesheet" type="text/css" href="../../public/css/style_manager_remove_dm.css">
+        <link rel="stylesheet" href="../../public/css/style_manager_sidenav.css">
+       
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
     </head>
 
-        <!--start of welcomeBox-->
+    <nav>    
+    <!--start of welcomeBox-->
         <div class="welcomeBox">       
             <a href="manager_home.php"><img src="../../public/img/manager2.jpg" class="icon"></a>
-            <h1>Welcome to Admin Page</h1>
+           <!-- -->
         </div> 
-            <table class="nav_table" border="0" width="100%" align="left" cellpadding="10" cellspacing="0">
-                <div class="navibar">
-            
-                    <div class="dropdown">
-                        <button class="dropbtn">Divisional Manager</button>
-                        <div class="dropdown-content">
-                            <a href="manager_dm.php">View Details</a>
-                            <a href="manager_dm_add.php">Add new Division Manager</a>
-                            <a href="manager_dm_removed.php">Removed DM List</a>
-                        </div>
-                    </div>
+    </nav>
+           <!-- <table class="nav_table" border="0" width="100%" align="left" cellpadding="10" cellspacing="0">-->
+                
 
-                    <div class="dropdown">
-                        <button class="dropbtn">Reports</button>
-                        <div class="dropdown-content">
-                            <a href="manager_view_reports.php">Monthly Reports</a>
-                            <a href="manager_view_reports.php">Final Reports</a>
-                            <a href="manager_products_removed.php">Removed Product List</a>
-                        </div>
-                    </div>
+            <div class="sidebar">
+                <div class="header1">My Menu</div>
+                <a href="manager_home.php">
+                    
+                    <span>Profile</span>
+                </a>
+                <a href="manager_view_reports.php">
+                    
+                    <span>Reports</span>
+                </a>
+                <a href="manager_dm.php">
+                    
+                    <span>Divisional Managers</span>
+                </a>
+                <a href="manager_orders.php">
+                    
+                    <span>Order Details</span>
+                </a>
+                <a href="manager_products.php">
+                    
+                    <span>Products</span>
+                </a>
+                <a href="manager_viewarticle.php">
+                    
+                    <span>Info Hub</span>
+                </a>
 
-                    <div class="dropdown">
-                        <button class="dropbtn">Products</button>
-                        <div class="dropdown-content">
-                            <a href="manager_products.php">View Details</a>
-                            <a href="manager_products_add.php">Add a Product</a>
-                            
-                        </div>
-                    </div>
-
-                    <div class="dropdown">
-                        <button class="dropbtn">Order Details </button>
-                        <div class="dropdown-content">
-                            <a href="">Ongoing Orders</a>
-                            
-                            <a href="" name="enter">completed orders</a>
-                            </form>
-                            
-                        </div>
-                    </div>
-
-                    <div class="dropdown">
-                        <button class="dropbtn">Info Hub</button>
-                        <div class="dropdown-content">
-                            <a href="manager_infohub.php">Add Article</a>
-                            
-                            <a href="manager_viewarticle.php" name="enter">View Articles</a>
-                            </form>
-                            
-                        </div>
-                    </div>
-                </div>
-            </table>
+               <!-- <script src="../../public/js/manager_navbar_active.js"></script>-->
+               <Script type="text/javascript">
+                    const currentLocation = location.href;
+                    const menuItem = document.querySelectorAll('a');
+                    const menuLength = menuItem.length;
+                    for (let i = 0; i < menuLength ; i++) {
+                        if(menuItem[i].href===currentLocation) {
+                            menuItem[i].className ="active"
+                        }
+                        
+                    }
+                </Script>
+                
+            </div>
                                  <!--end of welcomeBox-->
 
 
