3e5e24921e155fcdad2a4d6de786ffda695d3eec
madhavi125 <msellahannadi@gmail.com>
2021-03-18 17:00:50 +0530

    changed

--- src/php/bk_sidenavbar.php
+++ src/php/bk_sidenavbar.php
@@ -6,11 +6,11 @@
 
 <div class="sidenav">
  
-  <a href="beekeeperindex.php" style="background-color:#99a290"><font type="arial" >Bee-hive</font></a>
-  <a href="bk_harvest.php" style="background-color:#576452"><font type="arial">Harvest</arial></a>
-  <a href="bk_feeding.php" style="background-color:#2b3528"><font type="arial">Feeding</arial></a>
-  <a href="bk_infohub.php" style="background-color:#4c5246"><font type="arial">Info-Hub</arial></a>
-  <a href="bk_reports.php" style="background-color:#547454">Reports</a></li>
+  <a href="beekeeperindex.php" style="background-color:#847d73"><font type="arial" >Bee-hive</font></a>
+  <a href="bk_harvest.php" style="background-color:#2f1b0b"><font type="arial">Harvest</arial></a>
+  <a href="bk_feeding.php" style="background-color:#54544a"><font type="arial">Feeding</arial></a>
+  <a href="bk_infohub.php" style="background-color:#844414"><font type="arial">Info-Hub</arial></a>
+  <a href="bk_reports.php" style="background-color:#6c5459">Reports</a></li>
  
 </div>
 
