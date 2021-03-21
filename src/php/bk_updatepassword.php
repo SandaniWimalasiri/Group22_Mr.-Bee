8944c4ce91dc4c7857ac26d95d058134e089b0ca
madhavi125 <msellahannadi@gmail.com>
2021-03-21 12:41:07 +0530

    changed

--- src/php/bk_updatepassword.php
+++ src/php/bk_updatepassword.php
@@ -1,28 +1,65 @@
 <?php require_once('../../config/connect.php'); 
 session_start();
 
-$error="";
-		if(isset($_POST['submit'])){
-			if($_POST['password']!=$_POST['repassword']){	
-				$error="Password does not match";
-				
-			}
+$currentpassword_error=$newpassword_error=$repassword_error="";
+$currentpassword=$newpassword=$repassword="";
 
-
-if(isset($_POST['update'])){
-    
+    if(isset($_POST['enter'])){
  
+        if (empty($_POST["currentpassword"])) {
+            $currentpassword_error = "</br>*Current Password is required";
+          }else {
+
+            $currentpassword = test_input($_POST["currentpassword"]);
+
+            $sql = "SELECT userPassword FROM beekeeper WHERE  userID='".$_SESSION['userid']."'";
+            $result = mysqli_query($connection,$sql);
+            while($row = mysqli_fetch_array($result))
+            {
+              if($row['userPassword']!== $currentpassword){
+              $currentpassword_error = "</br>*Invalid Password";
+            }
+
+  
+           }
+          }
+
+       if (empty($_POST["newpassword"])) {
+            $newpassword_error = "</br>*New Password is required";
+          } else {
+            $newpassword = test_input($_POST["newpassword"]);
+          }
+       if (empty($_POST["repassword"])) {
+            $repassword_error = "</br>*Re-enter new password";
+          } else {
+            $repassword = test_input($_POST["repassword"]);
+
+            if($newpassword!== $repassword){
+              $repassword_error = "</br>*Please re-enter correct new password";
+            }   
+          }  
+
+             
           
-    $sql2= "UPDATE beekeeper SET userPassword ='".$_POST['userPassword']."' WHERE userID='".$_SESSION['userid']."'";
-    $result2 = mysqli_query($connection,$sql2);
-   if($result2){
- 
-}
-else{
-echo "failed";	
-}
+      if($currentpassword_error=='' and $newpassword_error=='' and $repassword_error=='' ){
+        
+        $sql2= "UPDATE beekeeper SET userPassword ='".$_POST['newpassword']."'where userID='".$_SESSION['userid']."'";
+        $result2 = mysqli_query($connection,$sql2);
+        $sql3 = "SELECT userPassword  FROM beekeeper where userID='".$_SESSION['userid']."'";
+        $result3 = mysqli_query($connection,$sql3);
+        $row=mysqli_fetch_assoc($result3);
+          }
 
-}
+       
+        }
+
+        function test_input($data) {
+          $data = trim($data);
+          $data = stripslashes($data);
+          $data = htmlspecialchars($data);
+          return $data;
+        }
+
 
 
    
@@ -32,7 +69,7 @@ echo "failed";
 
     <head>
 
-      <title>bee-hive</title>
+      <title>change password</title>
       <link rel="stylesheet" type="text/CSS" href="../../public/css/bk_style.css">
       <link rel="stylesheet" type="text/CSS" href="../../public/css/bk_catstyle.css">
 
@@ -47,20 +84,46 @@ echo "failed";
 
 
 <div class="main">
-
-
     <div class="bhivecontainer">
-    <p >Change Password</p>
-    
-    <center><img src="  ../../public/img/14.png" ></center>
-</br>
-</br>
+  
+        <form method="post" action="bk_updatepassword.php" >
+        <p >Change Password</p>
+        </br>
+    </br>
+        <div class="row">
+        <div class="col1">
+        <label for="currentpassword">Current Password</label>
+        </div>
+    <div class="col2">
+        <input type="password" name="currentpassword"  placeholder="Current Password" value="<?= $currentpassword?>" autofocus>
+        <span class="error"><?= $currentpassword_error?></span>
+    </div>
+        </div>
+        <div class="row">
+        <div class="col1">
+        <label for="newpassword">New Password</label>
+        </div>
+    <div class="col2">
+        <input type="password" name="newpassword"  placeholder="New Password" value="<?= $newpassword?>" autofocus>
+        <span class="error"><?= $newpassword_error?></span>
+    </div>
+        </div>
+        <div class="row">
+        <div class="col1">
+        <label for="repassword">Re-Enter Password</label>
+        </div>
+    <div class="col2">
+        <input type="password" name="repassword"  placeholder="Re-enter Password" value="<?= $repassword?>" autofocus>
+        <span class="error"><?= $repassword_error?></span>
+    </div>
+        </div>
+        <br/>
 
-<p>Password </p>
-<input type="password" name="password" placeholder="Enter your password" required/>
-<p>Re-enter the Password </p>
-<input type="password" name="repassword" placeholder="Enter your password" required /><br />
-
+        <div class="row">
+        <div class="c1" style="width: 870px">
+        <input type="submit" value="Change Password" name="enter" >
+        </div>
+        </form>
 
 
 <form  action="bk_profile.php" method="post" >
