5fd8e7ff7940b443bbf95fdf90fc23f1de7ade11
madhavi125 <msellahannadi@gmail.com>
2021-03-25 09:50:03 +0530

    changed

---
+++ src/php/bk_removebeehive.php
@@ -0,0 +1,30 @@
+<?php require_once('../../config/connect.php'); ?>
+<?php
+if(isset($_GET['BeehiveRecNo'])){
+$sql = "DELETE FROM beehive WHERE BeehiveRecNo = ".$_GET['BeehiveRecNo'];
+$result = mysqli_query($connection,$sql);
+
+
+if($result){
+
+    echo '<script>';
+    echo 'alert("Record Removed Successfully");';
+    echo 'window.location.href = "bk_viewbeehive.php";';
+    echo '</script>';
+    die();
+  }
+
+  else{
+
+    echo '<script>';
+    echo 'alert("Failed");';
+    echo 'window.location.href = "bk_viewbeehive.php";';
+    echo '</script>';
+    die();
+  }
+
+}
+
+
+?>
+
