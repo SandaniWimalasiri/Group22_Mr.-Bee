e8e784c17bbfc863b15f735db2ea01b2e3aaa3a3
madhavi125 <msellahannadi@gmail.com>
2021-03-28 19:31:37 +0530

    changed

--- src/php/bk_ffeedingreport.php
+++ src/php/bk_ffeedingreport.php
@@ -1,12 +1,15 @@
 <?php require_once('../../config/connect.php'); 
 session_start();
 
+
+
+
 		?>
 
 <html>
 
 	<head>	
-		<title>feeding report</title>
+		<title>Full Feeding Report</title>
       <link rel="stylesheet" type="text/CSS" href="../../public/css/bk_style.css">
       <link rel="stylesheet" type="text/CSS" href="../../public/css/bk_catstyle.css">
 	</head>
@@ -21,35 +24,40 @@ session_start();
 
 
 <div class="bhivecontainer">
+<p>Full Feeding Report</p>
+<br/><br/><br/>
 
-<br/><br/>
-			
+	
 
            <center> <table >
                 <tr style="background-color:#547454">
     
-                <th>Feeding Record no</th>
-                <th>Feeding date</th>
-                <th>Feeding time</th>
-                <th>Feeding type</th>
-                <th>Feeding amount</th>
+                <th>Feeding Record No</th>
+                <th>Date</th>
+                <th>Feeding Date</th>
+                <th>Feeding Time</th>
+                <th>Feeding Type</th>
+                <th>Feeding Amount</th>
                 
                 </tr>
+               
 
 			<?php
-if(isset($_GET['beehiveno2'])){
-$id = isset($_POST['id']) ? $_POST['id'] : ''; 
+if(isset($_SESSION['beehiveno'])){
 
-        
-$sql = "SELECT FeedingRecNo,fdate,ftime,feedingtype,famount FROM feeding where beehiveno='".$_GET['beehiveno2']."' and userID='".$_SESSION['userid']."'";
-mysqli_query($connection, $sql);
-$result = mysqli_query($connection,$sql);
+    $sql = "SELECT FeedingRecNo,date,fdate,ftime,feedingtype,famount,unit FROM feeding WHERE beehiveno='".$_SESSION['beehiveno']."' AND userID='".$_SESSION['userid']."' AND is_deleted=0 ";
+    mysqli_query($connection, $sql);
+    $result = mysqli_query($connection,$sql);
+
 
 		while($row=mysqli_fetch_assoc($result)){
             
             echo '<tr >';
             echo '<td>';
         echo $row['FeedingRecNo'];
+        echo '</td>';
+        echo '<td>';    
+        echo $row['date'];
         echo '</td>';
             echo '<td>';
         echo $row['fdate'];
@@ -61,18 +69,49 @@ $result = mysqli_query($connection,$sql);
         echo $row['feedingtype'];
         echo '</td>';
         echo '<td>';
-        echo $row['famount'];
+        echo $row['famount']." ".$row['unit'];
         echo '</td>';
 
         echo '</tr>';
-
+        
 		
-		}}?>
+        }}
+    
+  
+
+        echo '<form action="bk_mfeedingreport.php"  method="post" >';
+        echo '<div class="row">
+        <div class="c1" style="width:300px"><select name="mon" style="width:250px" ><option value="00">Select Month</option>';
+        echo '<option value="01">January</option>';
+        echo '<option value="02">February</option>';
+        echo '<option value="03">March</option>';
+        echo '<option value="04">April</option>';
+        echo '<option value="05">May</option>';
+        echo '<option value="06">June</option>';
+        echo '<option value="07">July</option>';
+        echo '<option value="08">August</option>';
+        echo '<option value="09">September</option>';
+        echo '<option value="10">October</option>';
+        echo '<option value="11">November</option>';
+        echo '<option value="12">December</option>';
+        echo '</select></div>';
+        echo '<div class="c2"><input type="submit" value="View Monthly Report >>" name="enter" ></div></div></form></br></br></br>';
+    
+
+
+    
+    
+    ?>
+    
+   
+        
+
+        
 
             </table></center>
 
             </br>
-    </br>
+    </br><br/><br/>
             <form  action="bk_reports.php" method="post" >
         <div class="row">
         <input type="submit" value="<< Back" name="back" >
