7e11ef9a806130707ef0a625f487f1b5aef96d00
madhavi125 <msellahannadi@gmail.com>
2021-03-28 11:01:30 +0530

    changed

--- src/php/login.php
+++ src/php/login.php
@@ -4,68 +4,58 @@
   session_start();
 
   if(isset($_POST['submit'])){
-    //save email and password into variables
-    $email =mysqli_real_escape_string($connection,$_POST['email']);
-    $password =mysqli_real_escape_string($connection,$_POST['password']);
 
-    //prepare database query
-    $query=mysqli_query($connection,"SELECT * FROM div_manager WHERE email='{$email}' AND pwd ='{$password}'  LIMIT 1");
-    $row=mysqli_fetch_array($query);
-    $type=$row['emp_status'];
-
-    $isexist=mysqli_query($connection,"SELECT * FROM div_manager WHERE email='{$email}' AND pwd ='{$password}'  LIMIT 1");
-    $check_user=mysqli_num_rows($isexist);
-
-    if($check_user==1){
-      $_SESSION["type"]=$row['emp_status'];
-      $_SESSION["email"]=$row['email'];
-
-      $user=mysqli_fetch_assoc($isexist);
-      $_SESSION["first_name"]=$user['first_name'];
-      $_SESSION["last_name"]=$user['last_name'];
-      
-      if($type=="Divisional_Manager"){
-        header('Location: divman.php');
-      }
-    }else{
-      echo "<script>alert('Invalid email or Password');</script>";
-    }
-
-  }
-
-  if(isset($_POST['submit'])){
-    //save email and password into variables
-    $email =mysqli_real_escape_string($connection,$_POST['email']);
-    $password =mysqli_real_escape_string($connection,$_POST['password']);
-
-    //prepare database query
-    $query=mysqli_query($connection,"SELECT * FROM manager WHERE email='{$email}' AND pwd ='{$password}'  LIMIT 1");
-    $row=mysqli_fetch_array($query);
-    $type=$row['emp_status'];
-
-    $isexist=mysqli_query($connection,"SELECT * FROM manager WHERE email='{$email}' AND pwd ='{$password}'  LIMIT 1");
-    $check_user=mysqli_num_rows($isexist);
-
-    if($check_user==1){
-      $_SESSION["type"]=$row['emp_status'];
-      $_SESSION["email"]=$row['email'];
-
-      $user=mysqli_fetch_assoc($isexist);
-      $_SESSION["first_name"]=$user['first_name'];
-      $_SESSION["last_name"]=$user['last_name'];
-      
-      if($type=="manager"){
-        header('Location: manager_home.php');
-      }
-    }
-
-  }
-
-  if(isset($_POST['submit'])){
-		$uname=$_POST['email'];
+    $email=$_POST['email'];
 		$pwd=$_POST['password'];
 
-		$sql="SELECT * FROM beekeeper WHERE username='".$uname."' AND userPassword='".$pwd."'";
+    $sql="SELECT * FROM div_manager WHERE email='".$email."' AND pwd='".$pwd."'";
+		$result=mysqli_query($connection,$sql);
+		
+		if($result->num_rows==1){
+			$manager=$result->fetch_assoc();
+
+      $_SESSION["first_name"]=$manager['first_name'];
+      $_SESSION["last_name"]=$manager['last_name'];
+      
+      if($result){
+				$redirect =  "divman.php";
+			
+                        }
+
+                        header('Location: ' . $redirect);
+    }
+
+  }
+
+  if(isset($_POST['submit'])){
+    
+    $email=$_POST['email'];
+		$pwd=$_POST['password'];
+
+    $sql="SELECT * FROM manager WHERE email='".$email."' AND pwd='".$pwd."'";
+		$result=mysqli_query($connection,$sql);
+		
+		if($result->num_rows==1){
+			$manager=$result->fetch_assoc();
+
+      $_SESSION["first_name"]=$manager['first_name'];
+      $_SESSION["last_name"]=$manager['last_name'];
+      
+      if($result){
+				$redirect =  "manager_home.php";
+			
+                        }
+
+                        header('Location: ' . $redirect);
+    }
+
+  }
+
+  if(isset($_POST['submit'])){
+		$email=$_POST['email'];
+		$pwd=$_POST['password'];
+
+		$sql="SELECT * FROM beekeeper WHERE userEmail='".$email."' AND userPassword='".$pwd."'";
 		$result=mysqli_query($connection,$sql);
 		
 		if($result->num_rows==1){
@@ -117,7 +107,7 @@
         <h1>Login Here</h1>
         <form name="loginForm" action="login.php"  method="post">
           <p>Username</p>
-          <input type="text" name="email" placeholder="Enter email" required>
+          <input type="text" name="email" placeholder="Enter Email" required>
           <p>Password</p>
           <input type="password" name="password" placeholder="Enter Password" required>
           <input type="submit" name="submit" value="Login">
