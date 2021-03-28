e296babc4cd54dc7c1ba6741ce698eac446f04d2
madhavi125 <msellahannadi@gmail.com>
2021-03-28 19:30:09 +0530

    changed

--- src/php/bk_fharvestreport.php
+++ src/php/bk_fharvestreport.php
@@ -1,12 +1,15 @@
 <?php require_once('../../config/connect.php'); 
 session_start();
 
+
+
+
 		?>
 
 <html>
 
 	<head>	
-		<title>harvest report</title>
+		<title>View Full Harvest Report</title>
       <link rel="stylesheet" type="text/CSS" href="../../public/css/bk_style.css">
       <link rel="stylesheet" type="text/CSS" href="../../public/css/bk_catstyle.css">
 	</head>
@@ -17,68 +20,109 @@ session_start();
 
 
 
+  
+
 
 <div class="bhivecontainer">
-<br/><br/>
-	<center>		
-     <table >
+<p>Full Harvest Report</p>
+<br/><br/><br/>
+
+	
+
+           <center> <table >
                 <tr style="background-color:#547454">
     
-                <th>Harvesting Record no</th>
-                <th>Harvesting date</th>
-                <th>Harvesting time</th>
-                <th>Harvested product type</th>
-                <th>Harvested amount</th>
+                <th>Harvesting Record No</th>
+                <th>Date</th>
+                <th>Harvesting Date</th>
+                <th>Harvesting Time</th>
+                <th>Harvested Product Type</th>
+                <th>Harvested Amount</th>
+                
+                
                 
                 </tr>
+               
 
-            <?php
-            
-            
-if(isset($_GET['beehiveno1'])){
-$id = isset($_POST['id']) ? $_POST['id'] : ''; 
+			<?php
+if(isset($_SESSION['beehiveno'])){
 
-        
-$sql = "SELECT HarvestRecNo,hdate,htime,producttype,amount FROM harvest where beehiveno='".$_GET['beehiveno1']."' and userID='".$_SESSION['userid']."'";
-mysqli_query($connection, $sql);
-$result = mysqli_query($connection,$sql);
+        $sql = "SELECT HarvestRecNo,date,hdate,htime,producttype,amount,unit FROM harvest where beehiveno='".$_SESSION['beehiveno']."' and userID='".$_SESSION['userid']."' and is_deleted=0 ";
+    mysqli_query($connection, $sql);
+    $result = mysqli_query($connection,$sql);
 
-		while($row=mysqli_fetch_assoc($result)){
-            //echo '<tr style="background-color:white">';
-            echo '<tr >';
-            echo '<td>';
-            echo $row['HarvestRecNo'];
-            echo '</td>';
-                echo '<td>';    
+    while($row=mysqli_fetch_assoc($result)){
+        //echo '<tr style="background-color:white">';
+        echo '<tr >';
+        echo '<td>';
+        echo $row['HarvestRecNo'];
+        echo '</td>';
+        echo '<td>';    
         echo $row['hdate'];
         echo '</td>';
-        echo '<td>';
-        echo $row['htime'];
-        echo '</td>';
-        echo '<td>';
-        echo $row['producttype'];
-        echo '</td>';
-        echo '<td>';
-        echo $row['amount'];
-        echo '</td>';
-        echo '</tr>';
+            echo '<td>';    
+    echo $row['hdate'];
+    echo '</td>';
+    echo '<td>';
+    echo $row['htime'];
+    echo '</td>';
+    echo '<td>';
+    echo $row['producttype'];
+    echo '</td>';
+    echo '<td>';
+    echo $row['amount']." ".$row['unit'];
+    echo '</td>';
+    echo '</tr>';
+            
+    }}
+        
 		
-		}}?>
-     </table></center>
+        
+    
+
+    echo '<form action="bk_mharvestreport.php"  method="post" >';
+    echo '<div class="row">
+    <div class="c1" style="width:300px"><select name="mon" style="width:250px" ><option value="00">Select Month</option>';
+    echo '<option value="01">January</option>';
+    echo '<option value="02">February</option>';
+    echo '<option value="03">March</option>';
+    echo '<option value="04">April</option>';
+    echo '<option value="05">May</option>';
+    echo '<option value="06">June</option>';
+    echo '<option value="07">July</option>';
+    echo '<option value="08">August</option>';
+    echo '<option value="09">September</option>';
+    echo '<option value="10">October</option>';
+    echo '<option value="11">November</option>';
+    echo '<option value="12">December</option>';
+    echo '</select></div>';
+    echo '<div class="c2"><input type="submit" value="View Monthly Report >>" name="enter" ></div></div></form></br></br></br>';
+        
+    ?> 
 
 
-</br>
-    </br>
+
+    
+    
+    
+   
+        
+
+        
+
+            </table></center>
+
+            </br>
+    </br><br/><br/>
             <form  action="bk_reports.php" method="post" >
         <div class="row">
         <input type="submit" value="<< Back" name="back" >
         </div>
         </form>
-
-
+</div>
 </div>	
 
-</div>
+
 
 	</body>
 </html>
