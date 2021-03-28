ceabcce868064b9f73b7b8f4009884de289ff835
madhavi125 <msellahannadi@gmail.com>
2021-03-28 19:30:54 +0530

    changed

--- src/php/bk_fbeehivereport.php
+++ src/php/bk_fbeehivereport.php
@@ -6,7 +6,7 @@ session_start();
 <html>
 
 	<head>	
-		<title>Full beehive report</title>
+		<title>Full Beehive Report</title>
       <link rel="stylesheet" type="text/CSS" href="../../public/css/bk_style.css">
       <link rel="stylesheet" type="text/CSS" href="../../public/css/bk_catstyle.css">
 	</head>
@@ -18,35 +18,36 @@ session_start();
 
 
   <div class="bhivecontainer">
-
-            <br/><br/>
-
+<p>Full Beehive Report</p>
+            <br/><br/><br/>
+     
            
             <center>
             <table >
                 <tr style="background-color:#547454">
                 <th>Beehive Record No</th>
-                <th>Start date</th>
-                <th>Inspection date</th>
-                <th>Inspection time</th>
-                <th>Active status and temperament of bees</th>
-                <th>Weight of beehive</th>
-                <th>Weather status</th>
-                <th>Changes made to beehive</th>
-                <th>Number of frames</th>
-                <th>Signs of diseases</th>
+                <th>Date</th>
+                <th>Start Date</th>
+                <th>Inspection Date</th>
+                <th>Inspection Time</th>
+                <th>Active Status</th>
+                <th>Temperament</th>
+                <th>Weight of Beehive</th>
+                <th>Weather Status</th>
+                <th>Changes Made to Beehive</th>
+                <th>Number of Frames</th>
+                <th>Signs of Diseases</th>
                 <th>Treatments</th>
-                <th>Status of queen bee</th>
-                <th>Number of bee colonies in beehive</th>
+                <th>Status of Queen Bee</th>
+                <th>Number of Bee Colonies in Beehive</th>
                 </tr>
 
 
 			<?php
-if(isset($_GET['beehiveno'])){
-
-
+if(isset($_SESSION['beehiveno'])){
         
-$sql = "SELECT BeehiveRecNo,sdate,idate,itime,actstatus,wbeehive,wstatus,cbeehive,noframes,disease,treatment,sqbee,bcolony FROM beehive WHERE beehiveno='".$_GET['beehiveno']."'  and userID='".$_SESSION['userid']."'";
+        
+$sql = "SELECT BeehiveRecNo,date,sdate,idate,itime,actstatus,wbeehive,temperament,wstatus,cbeehive,noframes,disease,treatment,sqbee,bcolony FROM beehive WHERE beehiveno='".$_SESSION['beehiveno']."'  and userID='".$_SESSION['userid']."' and is_deleted=0 ";
 mysqli_query($connection, $sql);
 $result = mysqli_query($connection,$sql);
 
@@ -54,6 +55,9 @@ $result = mysqli_query($connection,$sql);
         echo '<tr >';
         echo '<td>';
         echo $row['BeehiveRecNo'];
+        echo '</td>';
+        echo '<td>';    
+        echo $row['date'];
         echo '</td>';
         echo '<td>';
         echo $row['sdate'];
@@ -66,6 +70,9 @@ $result = mysqli_query($connection,$sql);
         echo '</td>';
         echo '<td>';
         echo $row['actstatus'];
+        echo '</td>';
+        echo '<td>';
+        echo $row['temperament'];
         echo '</td>';
         echo '<td>';
         echo $row['wbeehive']." Kg";
@@ -95,12 +102,35 @@ $result = mysqli_query($connection,$sql);
         echo '</tr>';
 
 		
-		}}?>
+                }}
+                
+                
+                
+                
+                echo '<form action="bk_mbeehivereport.php"  method="post" >';
+                echo '<div class="row">
+                <div class="c1" style="width:300px"><select name="mon" style="width:250px" ><option value="00">Select Month</option>';
+                echo '<option value="01">January</option>';
+                echo '<option value="02">February</option>';
+                echo '<option value="03">March</option>';
+                echo '<option value="04">April</option>';
+                echo '<option value="05">May</option>';
+                echo '<option value="06">June</option>';
+                echo '<option value="07">July</option>';
+                echo '<option value="08">August</option>';
+                echo '<option value="09">September</option>';
+                echo '<option value="10">October</option>';
+                echo '<option value="11">November</option>';
+                echo '<option value="12">December</option>';
+                echo '</select></div>';
+                echo '<div class="c2"><input type="submit" value="View Monthly Report" name="enter" ></div></div></form></br></br></br>';
+
+?>
 
             </table></center>
 
             </br>
-    </br>
+    </br><br/><br/>
             <form  action="bk_reports.php" method="post" >
         <div class="row">
         <input type="submit" value="<< Back" name="back" >
